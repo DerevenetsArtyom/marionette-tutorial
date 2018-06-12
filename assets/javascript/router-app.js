8c82a25b4dea062f0343f11719779bf644f9e3d7
DerevenetsArtyom <artem@comvoy.be>
2018-06-12 20:43:08 +0300

    Added views/list.js

--- assets/javascript/router-app.js
+++ assets/javascript/router-app.js
@@ -1,109 +1,31 @@
-const ToDoModel = Backbone.Model.extend({
-    defaults: {
-        assignee: '',
-        text: ''
-    },
+/// views/list.js
 
-    validate: function(attrs) {
-        var errors = {};
-        var hasError = false;
-        if (!attrs.assignee) {
-            errors.assignee = 'assignee must be set';
-            hasError = true;
-        }
-        if (!attrs.text) {
-            errors.text = 'text must be set';
-            hasError = true;
-        }
+// represents single blog item
+const Entry = Marionette.LayoutView.extend({
+    template: "#item",
+    tagName: "li",
 
-        if (hasError) {
-            console.log("errors", errors);
-            return errors;
-        }
+    triggers: {
+      click: "select:entry"
     }
 });
 
-
-/// views/list.js
-
-// represents single To-do item
-const TodoItemView = Marionette.LayoutView.extend({
-    tagName: "li",
-    template: "#todo-item"
-});
-
 // represents list of items
-const TodoListView = Marionette.CollectionView.extend({
-    childView: TodoItemView,
-    tagName: "ul"
+const BlogList = Marionette.CollectionView.extend({
+    childView: Entry,
+    tagName: "ul",
+
+    // todo: what a hell?
+    onChildViewSelectEntry: function (child, options) {
+        this.triggerMethod("select:entry", child.model)
+    }
 });
 
 
 /// views/form.js
 
-const FormView = Marionette.LayoutView.extend({
-    tagName: "form",
-    template: "#form",
-
-    ui: {
-        assignee: '#id_assignee',
-        text: '#id_text'
-    },
-
-    // when a jQuery event occurs, we can listen for it and fire a trigger
-    triggers: {
-        submit: 'add:todo:item'
-    },
-
-    modelEvents: {
-        change: 'render'
-    }
-});
-
-
 /// views/layout.js
 
-const Layout = Marionette.LayoutView.extend({
-    el: "#app-hook",
-    template: "#layout",
-
-    regions: {
-        form: ".form",
-        list: ".list"
-    },
-
-    collectionEvents: {
-        add: 'itemAdded'
-    },
-
-    onShow: function () {
-        const formView = new FormView({model: this.model});
-        const listsView = new TodoListView({collection: this.collection});
-
-        this.showChildView("form", formView);
-        this.showChildView("list", listsView);
-    },
-
-    onChildviewAddTodoItem: function (child) {
-        const data = {
-            assignee: child.ui.assignee.val(),
-            text: child.ui.text.val()
-        };
-        this.model.set(data);
-
-        if (this.model.isValid()) {
-            this.collection.add(this.model.toJSON());
-        }
-    },
-
-    // The method referenced in collectionEvents is called when the event is triggered
-    itemAdded: function () {
-        this.model.set({
-            assignee: '',
-            text: ''
-        });
-    }
-});
 
 /// driver.js - entrypoint for whole application
 
